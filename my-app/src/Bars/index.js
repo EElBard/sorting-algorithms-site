1bdf945dac3f31b2f41592566cfe619b66a83f4b
EElBard <101231052+EElBard@users.noreply.github.com>
2023-09-09 16:16:01 -0700

    Added click functionality and bar component

---
+++ my-app/src/Bars/index.js
@@ -0,0 +1,9 @@
+import './index.scss'
+
+const Bars = () => {
+    return (<>
+        <div className='bar' />
+    </>)
+}
+
+export default Bars
--- my-app/src/Home/index.js
+++ my-app/src/Home/index.js
@@ -1,12 +1,44 @@
 import Button from 'react-bootstrap/Button'
 import './index.scss'
+import Bars from '../Bars'
+import { useState } from 'react'
 
 const Home = () => {
+
+    const [numBars, setNumBars] = useState(20)
+
+    const handleClick = (action) => {
+        if(action == 'add-bar') {
+            setNumBars(numBars + 1)
+        }
+        else if(action == 'remove-bar') {
+            if(numBars > 0) {
+                setNumBars(numBars - 1)
+            }
+        }
+    }
+
     return (
         <>
-            <div className='header'>
-                <h1>Hello</h1>
-                <Button variant="primary">Button</Button> {''}
+            <div className='settings-button-container'>
+                <Button onClick={() => handleClick('add-bar')} variant='primary'>Add Bar</Button>
+                <Button onClick={() => handleClick('remove-bar')} variant='primary'>Remove Bar</Button>
+            </div>
+            <div className='sort-button-container'>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+                <Button variant="secondary">Selection Sort</Button>
+            </div>
+            <div className='bars-container'>
+                <Bars />
             </div>
         </>
     )
