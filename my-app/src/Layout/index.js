59d27f0279ca2ea2d9a65d6156d4140026fc90b2
EElBard <101231052+EElBard@users.noreply.github.com>
2023-09-09 15:38:24 -0700

    Refactored nav bar to be in layout without extra component

--- my-app/src/Layout/index.js
+++ my-app/src/Layout/index.js
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router-dom'
 import './index.scss'
-import Topnav from '../Topnav'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Layout = () => {
     return (<>
         <div className='App'>
-            <Topnav />
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar className="bg-body-tertiary">
+                    <Container>
+                        <Navbar.Brand>Sorting Algorithms</Navbar.Brand>
+                    </Container>
+                </Navbar>
+            </Container>
+        </Navbar>
             <Outlet />
         </div>
     </>)
