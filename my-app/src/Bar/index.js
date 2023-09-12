798371b9328b741f954f882e2bbfffd1f3748fcf
EElBard <101231052+EElBard@users.noreply.github.com>
2023-09-11 19:28:11 -0700

    Added color state to Bar component

--- my-app/src/Bar/index.js
+++ my-app/src/Bar/index.js
@@ -1,6 +1,12 @@
 import './index.scss'
+import { useState } from 'react'
 
 const Bar = ({height}) => {
+    const [color, setColor] = useState([])
+    const styles = {
+        color: color
+    }
+
     return (<>
         <div className="bar" style={{ height: height }}>
         </div>
